d6141148a1e5eb5508e0bdc7060817af76fa958e
404-Page-Found <Lucas20220605@gmail.com>
2026-08-01 10:54:37 +1000

    fix(notes): use secure note IDs
    Closes #529

--- src/features/notes.js
+++ src/features/notes.js
@@ -100,7 +100,7 @@
   }
 
   function generateNoteId() {
-    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+    return crypto.randomUUID();
   }
 
   function getUniqueTags() {
