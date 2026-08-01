a478e40a1d85cca6eb313180198dd3cf44b62fbd
404-Page-Found <Lucas20220605@gmail.com>
2026-08-01 13:11:19 +1000

    fix(todo): use secure UUIDs for todo identifiers
    Closes #539

--- src/features/todo.js
+++ src/features/todo.js
@@ -144,7 +144,7 @@
 
   // Generate unique ID for todos
   function generateTodoId() {
-    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+    return crypto.randomUUID();
   }
 
   // Date utilities
