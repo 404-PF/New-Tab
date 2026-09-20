dc40a88d31bb38a645632e833f1a04ba9fba3307
404-Page-Found <139850808+404-Page-Found@users.noreply.github.com>
2026-09-21 07:46:25 +1000

    fix(search): add suggestions accessibility label

--- src/core/languages.js
+++ src/core/languages.js
@@ -89,6 +89,7 @@ const translations = {
     recentSearches: 'Recent searches',
     clearSearchHistory: 'Clear history',
     enableSearchHistory: 'Enable search history',
+    searchSuggestionsAriaLabel: 'Search suggestions',
     newApp: 'New',
     feedback: 'Feedback',
     settings: 'Settings',
